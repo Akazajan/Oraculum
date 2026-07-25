1b8a643b9396b2ce282daa5d0c5c2c0bfcdf24bb
Maryermarh <Maryermarh@users.noreply.github.com>
2026-07-25 14:32:33 +0100

    feat(backend): Add notification preference management for users (Closes #39)

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -8,10 +8,12 @@ import { NotificationsController } from './notifications.controller';
 import { NotificationsGateway } from './gateway/notifications.gateway';
 import { CreateNotificationProvider } from './providers/create-notification.provider';
 import { FindNotificationsProvider } from './providers/find-notifications.provider';
+import { NotificationPreferencesModule } from '../notification-preferences/notification-preferences.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Notification]),
+    NotificationPreferencesModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
